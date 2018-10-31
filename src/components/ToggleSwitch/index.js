e7b62cc52584044a290ff4be65d1e4bf2eea500d
jerome.hardaway <jerome.hardaway@cbsinteractive.com>
2018-10-31 10:48:27 -0500

    TODO : Render Toggle Switch

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -7,13 +7,36 @@ import isFunction from 'lodash/isFunction';
 import './index.css';
 
 class ToggleSwitch extends Component {
+
 	state = { enabled: this.enabledFromProps()}
+	
 	isEnabled = () => this.state.enabled
+	
 	enabledFromProps() {
+	
 		let {enabled } = this.props;
 
 		enabled = isFunction(enabled) ? enabled : enabled;
+	
 		return isBoolean(enabled) && enabled;
+	}
+
+	toggleSwitch = evt => {
+		evt.persist();
+		evt.preventDefault();
+
+		const { onClick, onStateChanged } = this.props;
+
+		this.setState({ enabled: !this.state.enabled }, () => {
+			const state = this.state;
+
+			
+			const switchEvent = Object.assign(evt, { SWITCH_STATE: state });
+
+			
+			isFunction(onClick) && onClick(switchEvent);
+			isFunction(onStateChanged) && onStateChanged(state);
+		});
 	}
 }
 
